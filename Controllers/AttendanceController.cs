7bcce337b3c13e4be4624c7eb6c4dd8e3f639781
Igna Dwi Utami <ignadwiutami@gmail.com>
2020-03-10 21:53:17 +0700

    benerin attendance

--- Controllers/AttendanceController.cs
+++ Controllers/AttendanceController.cs
@@ -42,24 +42,37 @@ namespace HC_WEB_FINALPROJECT.Controllers
             ViewBag.items = get_employee;
             return View("AttendanceHome");
         }
+        public IActionResult AttendanceThisMonth(int Id)
+        {
+            var get_Attendance = from u in _AppDbContext.Attendances where u.EmployeeId == Id.ToString() select u;
+            ViewBag.Attendance = get_Attendance;
+            var get_employee = _AppDbContext.Employee.Find(Id);
+            ViewBag.Employee = get_employee;
+            return View();
+        }
         public IActionResult Create_ClockIn(int Id, string Remark)
         {
             var employee = _AppDbContext.Employee.Find(Id);
-            var obj = new Attendance()
+            var clckin = DateTime.Now;
+            var spesific_clockin = from a in _AppDbContext.Attendances where ((a.ClockIn.Day == clckin.Day && a.ClockIn.Month == clckin.Month && a.ClockIn.Year == clckin.Year) && (a.EmployeeId == (employee.Id).ToString())) select a;
+            if (!spesific_clockin.Any())
             {
-                ClockIn = DateTime.Now,
-                Remarks_in = Remark,
-                EmployeeId = Id.ToString()
-            };
-            _AppDbContext.Attendances.Add(obj);
-            _AppDbContext.SaveChanges();
+                var obj = new Attendance()
+                {
+                    ClockIn = DateTime.Now,
+                    Remarks_in = Remark,
+                    EmployeeId = Id.ToString()
+                };
+                _AppDbContext.Attendances.Add(obj);
+                _AppDbContext.SaveChanges();
+            }
             return RedirectToAction("AttendanceHome", "Attendance");
         }
         public IActionResult Create_ClockOut(int Id, string Remark)
         {
-            var employee =  _AppDbContext.Employee.Find(Id);
+            var employee = _AppDbContext.Employee.Find(Id);
             var clckout = DateTime.Now;
-            var spesific_clockin = from a in _AppDbContext.Attendances where ((a.ClockIn.Day == clckout.Day  && a.ClockIn.Month == clckout.Month && a.ClockIn.Year == clckout.Year) && (a.EmployeeId == (employee.Id).ToString())) select a;
+            var spesific_clockin = from a in _AppDbContext.Attendances where ((a.ClockIn.Day == clckout.Day && a.ClockIn.Month == clckout.Month && a.ClockIn.Year == clckout.Year) && (a.EmployeeId == (employee.Id).ToString())) select a;
             if (spesific_clockin.Any())
             {
                 var obj = spesific_clockin.First();
---
+++ Models/AttendancePaging.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HC_WEB_FINALPROJECT.Models
+{
+    public class AttendancePaging
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Day { get; set; }
+    }
+}
